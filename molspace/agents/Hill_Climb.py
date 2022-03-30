0682042cb231eba13dd7b2ad8de275aa30cbc091
mona <shubhangi.dutta@research.iiit.ac.in>
2022-03-30 23:59:04 +0530

    added to agents

---
+++ molspace/agents/Hill_Climb.py
@@ -0,0 +1,8 @@
+"""
+This module wis responsible for the implementation of the 
+Monte Carlo Tree Search.
+
+The module should plug into an environment interfacing with it's action-step
+interface and attempt to maximally explore relevant parts of said environment
+with minimum number of search calls.
+"""
